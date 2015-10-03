7be1dd01e4009c0c0bc5b1d4cdf37a716598d2ff
lighter <lighter.idv@gmail.com>
2015-10-03 06:12:19 +0000

    finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    
+    assert_no_difference 'User.count' do
+      post users_path, user: {name: "",
+                              email: "test@test.com",
+                              password: "1234",
+                              password_confirmation: "123"
+      }
+    end
+    assert_template 'users/new'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    name = "test123"
+    email = "test123@test.com"
+    password = "test1234"
+    
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user:{
+        name: name,
+        email: email,
+        password: password,
+        password_confirmation: password
+      }
+    end
+    
+    assert_template 'users/show'
+  end
+  
+end
